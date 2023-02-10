1796e6c5ccfbf0a279845c085a1cd2855ea29e8e
deviant <devi4nt@Shaukaths-MacBook-Pro.local>
2023-02-10 09:14:31 +0500

    initial new commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-graphcms`,
+      options: {
+        endpoint: `https://api-ap-northeast-1.hygraph.com/v2/cldxzwyd00sky01rrf6dt8ypy/master`,
+      },
+    },
+  ],
+};
